06730465a1b2046dd6f76178d36a5fbff3d19706
JTucker477 <65978098+JTucker477@users.noreply.github.com>
2021-04-27 16:24:41 -0400

    fiexed random solution and implemented reapeated random, repeated random gives seg fault

--- kk.cc
+++ kk.cc
@@ -8,64 +8,71 @@
 #include <cmath> 
 #include <cstdlib>
 #include <tuple>
+#include <numeric>
 using namespace std;
+int max_iter = 1; 
+typedef long long ll;
 
 // Returns std array that has a 100 instances of 100 random numbers to input into our algorithms 
-array<array <unsigned long long , 100>,100> rand_array(){
 
-    int size = 100;
+array<array <ll , 100>,100> rand_array(){
+    array<array <ll , 100>,100>  final;
+    random_device rand_dev;
+    mt19937 generator(rand_dev());
+    std::uniform_int_distribution<long> distribution(1, pow(10,12));
 
-    array <array <unsigned long long, 100>,100> final;
+    for (int j = 0; j < 100; j++ ){
+        array <ll, 100> temp;
 
-    // https://stackoverflow.com/questions/37396278/how-to-generate-very-large-random-number-in-c/37397139
-
-    /* Seed */
-    std::random_device rd;
-
-    /* Random number generator */
-    std::default_random_engine generator(rd());
-
-    /* Distribution on which to apply the generator */
-    std::uniform_int_distribution<long long unsigned> distribution(0,0xFFFFFFFFFFFFFFFF);
-
-
-    for (int j = 0; j < size; j++ ){
-        array <unsigned long long, 100> temp;
-
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < 100; i++) {
             temp[i] = distribution(generator) ;
+         
         }
         final[j] = temp;
     }
+
+}
+
+// array<array <ll , 100>,100> rand_array(){
+
+//     int size = 100;
+
+//     array <array <ll, 100>,100> final;
+
+//     // https://stackoverflow.com/questions/37396278/how-to-generate-very-large-random-number-in-c/37397139
+
+//     /* Seed */
+//     random_device rd;
+
+//     /* Random number generator */
+//     // std::default_random_engine generator(rd())
+
+//     /* Distribution on which to apply the generator */
+//     // std::uniform_int_distribution<long long > distribution(1,pow(10,12));
+
+
+
   
  
-    return final;
-}
-array <unsigned long long , 100> karmarkar(array<array <unsigned long long , 100>,100> input){
+//     return final;
+// }
+array <ll , 100> karmarkar(array<array <ll , 100>,100> input){
 
 
 
-    array <unsigned long long , 100> final_residues ;
-
-   
-
-    // Example: residue should be 2. 
-    // input[0][0] = 9;
-    // input[0][1] = 5;
-    // input[0][2] = 3;
-    // input[0][3] = 15;
+    array <ll , 100> final_residues ;
     
     // For each of the 100 instances 
     for (int i = 0 ; i < 100 ; i++){
 
 
-        unsigned long long current_distance;
+        ll current_distance;
         int numby = 0;
         while(true){
             numby += 1; 
         
-            array <unsigned long long , 100>::iterator first_max;
-            array <unsigned long long , 100>::iterator second_max;
+            array <ll , 100>::iterator first_max;
+            array <ll , 100>::iterator second_max;
 
             // Find Largest number
             first_max = max_element(input[i].begin(), input[i].end());
@@ -105,44 +112,149 @@ array <unsigned long long , 100> karmarkar(array<array <unsigned long long , 100
 
 }   
 
-// Returns two random subsets of the inputed array. STILL TO DO
-// need to return two sets (return two things!)
-// initialize 
-// each array is at most 100 but should be 
-// test
-pair<vector <unsigned long long >, vector <unsigned long long> > create_random_set_division(array <unsigned long long , 100> input){
-    // array <unsigned long long , 100> s1;
-    // array <unsigned long long , 100> s2;
-    vector <unsigned long long> s1;
-    vector <unsigned long long> s2;
+
+
+array <ll, 100 > random_solution(){
+    // array <ll , 100> s1;
+    // array <ll , 100> s2;
+    array <ll, 100> sol;
 
     // add elements to random subset
     for (int i = 0; i < 100; i++)
     {
         if (rand()%2 == 0)
         {
-            s1.push_back(input[i]);
+            sol[i] = -1;
         }
         // if not in random subset, add to another subset
         else {
-            s2.push_back(input[i]);
+            sol[i] = 1;
         }
     }
-    return make_pair(s1,s2);
+    return sol;
 }
 
+array <ll , 100> repeated_random (array<array <ll , 100>,100> input){
+    array <ll , 100> final; 
+    
+    // For each of the 100 instances of the problem
+    for (int i = 0; i < 1; i++){
+        array <ll , 100> solution;
+        array <ll , 100> multiply_result;
+        solution = random_solution();
+        multiply_result = solution; 
+        ll lowest_residue;
+        // element wise multiplication of input Result is stored in multiply_result. input is unchanged
+        transform( input[i].begin(), input[i].end(),
+                    multiply_result.begin(), multiply_result.end(),  
+                    std::multiplies<int>() );
+        lowest_residue = accumulate(begin(multiply_result), end(multiply_result),0);
 
-    // for (int i = 0; i < 100 ; i++){
-    // for (int i = 0; i < 100; i++){
-    //     if (rand()%2 == 0){
+        // The actual iteration 
+        for (int i = 0; i < max_iter ; i ++ ){
+            
+           
+            // Try a new random solution
+            array <ll , 100> new_sol = random_solution();
+            array <ll , 100> temp_multiply_result;
+            temp_multiply_result = new_sol;
+            // Find residue result 
+            transform( input[i].begin(), input[i].end(),
+                    temp_multiply_result.begin(), temp_multiply_result.end(),  
+                    std::multiplies<int>() );
 
-    //     }
-    //     else{
-    //         s1
-    //     }
-    // }
-    // }
+            printf("temp_multiply_result[0] %lld\n",temp_multiply_result[0] );
+            
+            ll current_residue = accumulate(begin(temp_multiply_result), end(temp_multiply_result),0);
 
+            if (current_residue < lowest_residue){
+                lowest_residue = current_residue;
+            }
+        }
+
+        final[i] = lowest_residue;
+    }
+    
+    return final; 
+}
+
+// hill climbing
+
+//  simmulated annealing
+
+
+int main(){
+    srand(time(NULL));
+    array<array <ll , 100>,100> chicken = rand_array();
+    // karmarkar(chicken);
+    // // for (int i = 0; i < 11; i++){
+    // //     printf("number: %llu\n", chicken[0][i]); 
+    // // }
+    // printf("try her out: %d\n", abs(-9));
+    // array <ll , 100>::iterator first_max;
+    // first_max = max_element(chicken[0].begin(), chicken[0].end());
+    // printf("max: %llu\n", (long long unsigned) *max_element(chicken[0].begin(), chicken[0].end()));
+    array <ll, 100> final;
+    printf("hihihihihi\n");
+    final = repeated_random(chicken);
+
+    printf("hey girl\n");
+    printf("result: %lld \n", final[0]);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// array <int, 3> v1 = {1,2,3};
+//     array <int, 3> v2 = {2,3,4}; 
+//     array <int, 3> v3 = v1;
+//     v2[0] = 0; 
+//     std::transform( v1.begin(), v1.end(),
+//                 v2.begin(), v2.begin(),  // assumes v1,v2 of same size > 1, 
+//                                           //       v one element smaller
+//                 std::multiplies<int>() );
+//     printf("hi %d\n", v1[0]);
+//     printf("hi %d\n", v1[1]);
+//     printf("hi %d\n", v1[2]);
+
+
+// Returns two random subsets of the inputed array. STILL TO DO
+// need to return two sets (return two things!)
+// initialize 
+// each array is at most 100 but should be 
+// test
+// pair<vector <ll >, vector <ll> > two_random_set(array <ll , 100> input){
+//     // array <ll , 100> s1;
+//     // array <ll , 100> s2;
+//     vector <ll> s1;
+//     vector <ll> s2;
+
+//     // add elements to random subset
+//     for (int i = 0; i < 100; i++)
+//     {
+//         if (rand()%2 == 0)
+//         {
+//             s1.push_back(input[i]);
+//         }
+//         // if not in random subset, add to another subset
+//         else {
+//             s2.push_back(input[i]);
+//         }
+//     }
+//     return make_pair(s1,s2);
+// }
 
 // Heuristics
 
@@ -157,31 +269,19 @@ pair<vector <unsigned long long >, vector <unsigned long long> > create_random_s
 // Maybe create two random subsets within
 
 //pseudocode
-vector a;
-for (int i = 0; i < 100; i++)
-{
-    a.push_back(rand())
-}
+// vector a;
+// for (int i = 0; i < 100; i++)
+// {
+//     a.push_back(rand())
+// }
 
-array <unsigned long long , 100> repeated_random(){
-    // Create random solution
-}
+// array <ll , 100> repeated_random(){
+//     // Create random solution
+// }
+
+// John's repeated random:
+
+// pair<vector <ll >, vector <ll> > random_neighbor(pair<vector <ll >, vector <ll> >){
 
 
-// hill climbing
-
-//  simmulated annealing
-
-
-int main(){
-    array<array <unsigned long long , 100>,100> chicken = rand_array();
-    karmarkar(chicken);
-    // for (int i = 0; i < 11; i++){
-    //     printf("number: %llu\n", chicken[0][i]); 
-    // }
-    printf("try her out: %d\n", abs(-9));
-    array <unsigned long long , 100>::iterator first_max;
-    first_max = max_element(chicken[0].begin(), chicken[0].end());
-    // printf("max: %llu\n", (long long unsigned) *max_element(chicken[0].begin(), chicken[0].end()));
-    
-} 
+// }
